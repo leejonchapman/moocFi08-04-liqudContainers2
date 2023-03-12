8b6020e8621b9699f17c24ce9e2904db8b2ec47b
Lee Chapman <me@leejonchapman.com>
2023-03-12 12:33:42 +0000

    1

--- src/Container.java
+++ src/Container.java
@@ -1,3 +1,27 @@
 public class Container {
-    
+    int volume;
+
+    public Container() {
+    }
+
+    public int contains() {
+        return this.volume;
+    }
+
+    //which adds the amount of liquid given as a parameter to the container. If the amount is negative, no liquid is added. A container can hold maximum of 100 units of liquid.
+    public void add(int amount) {
+
+    }
+
+    //which removes the amount of liquid given as a parameter from the container. If the amount is negative, no liquid is removed. A container can never hold less than 0 units of liquid.
+    public void remove(int amount) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Container{" +
+                "volume=" + volume +
+                '}';
+    }
 }
