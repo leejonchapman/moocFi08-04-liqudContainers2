1baed0eb775cca5259d34cabd944ca183493503a
Lee Chapman <me@leejonchapman.com>
2023-03-12 13:15:48 +0000

    1

--- src/Container.java
+++ src/Container.java
@@ -8,20 +8,30 @@ public class Container {
         return this.volume;
     }
 
-    //which adds the amount of liquid given as a parameter to the container. If the amount is negative, no liquid is added. A container can hold maximum of 100 units of liquid.
+    //If the amount is negative, no liquid is added. A container can hold maximum of 100 units of liquid.
     public void add(int amount) {
-
+        if (amount < 1) {
+            // Do nothing
+        } else if (this.volume + amount > 100) {
+            this.volume = 100;
+        } else {
+            this.volume += amount;
+        }
     }
 
-    //which removes the amount of liquid given as a parameter from the container. If the amount is negative, no liquid is removed. A container can never hold less than 0 units of liquid.
+    //If the amount is negative, no liquid is removed. A container can never hold less than 0 units of liquid.
     public void remove(int amount) {
-
+        if (amount < 1) {
+            // Do nothing
+        } else if (this.volume - amount < 1) {
+            this.volume = 0;
+        } else {
+            this.volume -= amount;
+        }
     }
 
     @Override
     public String toString() {
-        return "Container{" +
-                "volume=" + volume +
-                '}';
+        return this.volume + "/100";
     }
 }
---
+++ src/UserInterface.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    Scanner scanner;
+
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        boolean runMenu = true;
+
+        while (runMenu) {
+
+        }
+    }
+}
